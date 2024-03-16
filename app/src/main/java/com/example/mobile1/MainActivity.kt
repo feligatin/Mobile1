dcbbce2c1fe48bd94bcdcd389e538abea23c2ad2
Felipe Serrano <feligatin@gmail.com>
2024-03-16 13:02:16 -0500

    push init

--- app/src/main/java/com/example/mobile1/MainActivity.kt
+++ app/src/main/java/com/example/mobile1/MainActivity.kt
@@ -31,6 +31,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    //text
     Text(
         text = "Hello $name!",
         modifier = modifier
